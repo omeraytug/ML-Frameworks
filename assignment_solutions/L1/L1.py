50155f811fdd80ac3a2dbca211b73bd1b748dcaa
Omer Aytug <aytugomer00@gmail.com>
2026-01-27 14:27:58 +0100

    task 1 solutions for l1

--- assignment_solutions/L1/L1.py
+++ assignment_solutions/L1/L1.py
@@ -10,11 +10,32 @@ Instructions:
 """
 
 # Task 1: Vector and matrix basics (NumPy)
+
+import numpy as np
 # TODO: Create two vectors (length 3) and compute:
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+
 # - dot product
+dot_product = np.dot(v1, v2) 
+print("Dot product:", dot_product)
+
 # - L2 norm
+l2_v1 = np.linalg.norm(v1)
+l2_v2 = np.linalg.norm(v2)
+
+print("L2 norm v1:", l2_v1)
+print("L2 norm v2:", l2_v2)
+
 # - cosine similarity
+cosine_similarity = dot_product / (l2_v1 * l2_v2)
+print("Cosine similarity:", cosine_similarity)
+
 # TODO: Create a 2x3 matrix and multiply it by a length-3 vector
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6]])
+matrix_vector_product = matrix @ v1
+print("Matrix-vector product:\n", matrix_vector_product)
 
 # Task 2: Eager vs graph execution
 # TODO: Write a small function f(x) = x^3 + 2x
